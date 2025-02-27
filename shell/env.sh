58083f1cb5184868a96dd32f26bd2f64d3647194
Yu-Ting Shi <the.yutings@gmail.com>
2025-02-28 02:21:38 +0800

    Add script for environment variable settings

---
+++ shell/env.sh
@@ -0,0 +1,8 @@
+# XDG Base Directory Specification
+# https://specifications.freedesktop.org/basedir-spec/basedir-spec-latest.html
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CACHE_HOME="$HOME/.cache"
+export XDG_DATA_HOME="$HOME/.local/share"
+
+export PATH=$HOME/bin:$PATH
+export PATH=$HOME/.local/bin:$PATH
